79aa6a78679a3da0e17d48ef9c6b371f67ab6cf1
Artemis Papanikolaou <artemispapanik@gmail.com>
2021-03-08 20:40:57 +0000

    JS - Sum of positives updated

--- JS-Sum_of_positives/sum.js
+++ JS-Sum_of_positives/sum.js
@@ -14,3 +14,15 @@ function positiveSum(arr) {
     return sum;
     }
   }
+
+  function positiveSum(arr) {
+    var sum = 0;
+    
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] > 0) {
+        sum += arr[i]
+      }
+    }
+    return sum; 
+   
+    }
