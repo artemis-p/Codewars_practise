7f19ff24fb72b277d1a35a54227f7bdffb32d3b2
Artemis Papanikolaou <artemispapanik@gmail.com>
2021-03-09 15:16:14 +0000

    Removed spaces and debugged if statement

--- JS - Vowel Counts/vowelCounts.js
+++ JS - Vowel Counts/vowelCounts.js
@@ -3,12 +3,13 @@
 function getCount(str) {
     var vowelsCount = 0;
     var vowels = ["a", "e", "i", "o", "u"];
-  
+    
+    var str = str.replace(/\s/g, '');
     var newStr = str.split("");
    
     for(var i = 0; i < newStr.length; i++){
       for (var j = 0; j < vowels.length; j++) {
-        if (newStr[i] == vowels[i])
+        if (newStr[i] == vowels[j])
           vowelsCount +=1;
       }
     }
